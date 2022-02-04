0c36abdcfcbd4ee35a2893366fe53b4b363757e0
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2022-02-04 08:39:58 +0100

    fix: only process links to whitelisted sites

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,9 +1,10 @@
 function redirect() {
 	const params = new URL(location.href).searchParams;
 	// There should only be one site to redirect, so just grab the first one and load it
-	for (const [site, hash] of params.entries()) {
+	const [site, hash] = params.entries().next().value ?? [];
+	const whitelist = new Set(["pastebin", "pobbin", "poeninja"]);
+	if (whitelist.has(site)) {
 		window.location.href = `pob:${site}/${hash}`;
-		break;
 	}
 }
 redirect();
