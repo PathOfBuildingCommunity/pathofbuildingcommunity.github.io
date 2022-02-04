4cb59ede4a79f2162c906bd1b2bb3bc11f14fb1c
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2022-02-04 06:35:19 +0100

    refactor: idiomatic js

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,9 +1,7 @@
 function redirect() {
 	const params = new URL(location.href).searchParams;
 	// There should only be one site to redirect, so just grab the first one and load it
-	for(var pair of params.entries()) {
-		window.location.href = "pob:" + pair[0] + "/" + pair[1];
-		break;
-	}
+	const [site, hash] = params.entries().next();
+	window.location.href = `pob:${site}/${hash}`;
 }
 redirect();
