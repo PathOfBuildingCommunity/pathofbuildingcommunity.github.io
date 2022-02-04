ea06f99f4af13dd767d9003d621873d86e0354db
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2022-02-04 07:34:35 +0100

    fix: entries have no next method

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,7 +1,9 @@
 function redirect() {
 	const params = new URL(location.href).searchParams;
 	// There should only be one site to redirect, so just grab the first one and load it
-	const [site, hash] = params.entries().next();
-	window.location.href = `pob:${site}/${hash}`;
+	for (const [site, hash] of params.entries()) {
+		window.location.href = `pob:${site}/${hash}`;
+		break;
+	}
 }
 redirect();
